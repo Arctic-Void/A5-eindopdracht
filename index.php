eb93b1a754c0ab38fdb251b192495017262fcd95
daniongithub <danivandendobbelsteen@gmail.com>
2023-01-20 09:04:08 +0100

    rewrew

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="nl">
+
 <head>
     <meta charset="utf-8">
     <title>Fair play in de VOORBEELD-DIVISIE</title>
@@ -11,37 +12,59 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <!-- Hier worden de fonts al voor je ingeladen! -->
-    <link href="https://fonts.googleapis.com/css2?family=Faster+One&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&family=Roboto+Mono:wght@400;500&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Faster+One&family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500&family=Roboto+Mono:wght@400;500&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
 
     <div class="container">
-        <header>
-            <h1>Voorbeeld Divisie</h1>
-            <p class="subtitle">Fair-play website</p>
-        </header>
+        <div class="wrapper">
+            <header>
+                <h1>Voorbeeld Divisie</h1>
+                <p class="subtitle">Fair-play website</p>
+            </header>
 
-        <main>
-            <p>De SPORT-bond ziet toe op een eerlijk verloop van de competitie. Onze topsporters dienen een voorbeeld te zijn voor de vele amateurs in de sport. Daarom streven we naar <em>fair play</em>; een sportieve omgang met elkaar. Daar hoort ook bij dat er weinig overtredingen plaatsvinden tijdens een wedstrijd. Op deze website geven we inzicht in het verloop van de competitie tot nu toe. U ziet de wedstrijden met de minste overtredingen, maar voor bewustwording brengen we ook de wedstrijden met de <em>meeste</em> overtredingen in beeld.</p>
-            <div class="row">
-                <div class="box half">
-                    <h2>Aantal overtredingen:</h2>
-                    <p class="number"><?php echo 0 ?></p>
+            <main>
+                <p>De SPORT-bond ziet toe op een eerlijk verloop van de competitie. Onze topsporters dienen een
+                    voorbeeld te zijn voor de vele amateurs in de sport. Daarom streven we naar <em>fair play</em>; een
+                    sportieve omgang met elkaar. Daar hoort ook bij dat er weinig overtredingen plaatsvinden tijdens een
+                    wedstrijd. Op deze website geven we inzicht in het verloop van de competitie tot nu toe. U ziet de
+                    wedstrijden met de minste overtredingen, maar voor bewustwording brengen we ook de wedstrijden met
+                    de <em>meeste</em> overtredingen in beeld.</p>
+                <div class="row">
+                    <div class="box half">
+                        <h2>Aantal overtredingen:</h2>
+                        <p class="number">
+                            <?php echo 3115 ?>
+                        </p>
 
-                    <h2>Aantal overtredingen:</h2>
-                    <p class="number"><?php echo 0 ?></p>
+                        <h2>Gemiddeld per wedstrijd:</h2>
+                        <p class="number"><?php echo 8 ?></p>
+
+                    </div>
                 </div>
-            </div>
-        </main>
 
-        <footer>
-            <p>Deze website is gemaakt in het kader van een praktijkopdracht bij de opleiding Software Developer, Curio Breda.</p>
-            <p>&copy; Team X, Pietje Puk en Jan Smit.</p>
-        </footer>
+                <div class="column">
+                    <div class="box">
 
+                        <h2>Wedstrijden met de meeste overtredingen:</h2>
+
+
+                        <h2>Wedstrijden met 1 of minder overtredingen in de laatste 21 dagen:</h2>
+                    </div>
+                </div>
+            </main>
+
+            <footer>
+                <p>Deze website is gemaakt in het kader van een praktijkopdracht bij de opleiding Software Developer,
+                    Curio Breda.</p>
+                <p>&copy; Berend, Dani en Zouhir.</p>
+            </footer>
+        </div>
     </div>
 
 </body>
-</html>
 
+</html>
